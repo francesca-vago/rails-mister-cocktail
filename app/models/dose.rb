7b00eb0adbc487a0525600c6db46d832333098a7
Francesca Vago <vago.francesca@gmail.com>
2019-08-15 14:01:14 +0100

    added validations to models

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,9 @@
 class Dose < ApplicationRecord
+  validates :description, presence: true
+  validates :cocktail_id, presence: true
+  validates :ingredient_id, presence: true
+  validates :cocktail_id, uniqueness: { scope: :ingredient_id }
+
   belongs_to :cocktail
   belongs_to :ingredient
 end
